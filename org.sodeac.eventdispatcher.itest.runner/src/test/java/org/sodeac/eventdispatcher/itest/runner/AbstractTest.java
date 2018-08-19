08c963b8a7d43d67a4e2e3b2e89ad7602007d961
spalarus <s.palarus@googlemail.com>
2018-08-19 11:01:33 +0200

    version update

--- org.sodeac.eventdispatcher.itest.runner/src/test/java/org/sodeac/eventdispatcher/itest/runner/AbstractTest.java
+++ org.sodeac.eventdispatcher.itest.runner/src/test/java/org/sodeac/eventdispatcher/itest/runner/AbstractTest.java
@@ -142,10 +142,10 @@ public abstract class AbstractTest
 			mavenBundle("org.sodeac","org.sodeac.multichainlist","0.9.0-SNAPSHOT").start(),
 			mavenBundle("com.googlecode.json-simple", "json-simple", "1.1.1").start(),
 			mavenBundle("com.google.guava", "guava", "25.1-jre").start(),
-			reactorBundle("org.sodeac.eventdispatcher.api","0.9.3").start(),
-			reactorBundle("org.sodeac.eventdispatcher.provider","0.9.5").start(),
-			reactorBundle("org.sodeac.eventdispatcher.extension.jmx","0.9.5").start(),
-			reactorBundle("org.sodeac.eventdispatcher.common","0.9.3").start(),
+			reactorBundle("org.sodeac.eventdispatcher.api","0.9.4").start(),
+			reactorBundle("org.sodeac.eventdispatcher.provider","0.9.6").start(),
+			reactorBundle("org.sodeac.eventdispatcher.extension.jmx","0.9.6").start(),
+			reactorBundle("org.sodeac.eventdispatcher.common","0.9.4").start(),
 			reactorBundle("org.sodeac.eventdispatcher.itest.components","0.9.0").start()
 		};
 	}
