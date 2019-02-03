98a5545c49c2b12f7e749d0d575b0cfaea33dd07
spalarus <s.palarus@googlemail.com>
2019-02-03 08:45:58 +0100

    use checked method invoke

--- org.sodeac.eventdispatcher.api/src/main/java/org/sodeac/eventdispatcher/api/IPropertyBlock.java
+++ org.sodeac.eventdispatcher.api/src/main/java/org/sodeac/eventdispatcher/api/IPropertyBlock.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2018 Sebastian Palarus
+ * Copyright (c) 2017, 2019 Sebastian Palarus
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -110,13 +110,12 @@ public interface IPropertyBlock
 		
 		computeProcedure(new IPropertyBlockAtomicProcedure()
 		{
-			@SuppressWarnings("unchecked")
 			@Override
 			public void accept(IPropertyBlock propertyBlock)
 			{
 				if(propertyBlock.containsKey(key))
 				{
-					valueContainer.accept((T)propertyBlock.getProperty(key));
+					valueContainer.accept(propertyBlock.getProperty(key,resultClass));
 				}
 				else
 				{
