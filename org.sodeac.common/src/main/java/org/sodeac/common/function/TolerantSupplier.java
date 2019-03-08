601387bdd5e287974d3725df5cdb5c6df626f0cd
spalarus <s.palarus@googlemail.com>
2019-03-08 14:39:12 +0100

    rename refactor supply and add tests

--- org.sodeac.common/src/main/java/org/sodeac/common/function/TolerantSupplier.java
+++ org.sodeac.common/src/main/java/org/sodeac/common/function/TolerantSupplier.java
@@ -13,8 +13,51 @@ package org.sodeac.common.function;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
-public class TolerantSupply
+public class TolerantSupplier<T> implements Supplier<T>
 {
+	private int attemptCount = 1;
+	private int timeToSleepValue = 1; 
+	private TimeUnit timeToSleepUnit = TimeUnit.MILLISECONDS;
+	private T defaultValue = null;
+	private Supplier<T> internSupplier = null;
+	
+	private TolerantSupplier()
+	{
+		super();
+	}
+	
+	public static <T> TolerantSupplier<T> forSupplier(Supplier<T> internSupplier)
+	{
+		TolerantSupplier<T> tolerantSupply = new TolerantSupplier<>();
+		tolerantSupply.internSupplier = internSupplier == null ? () -> null : internSupplier;
+		return tolerantSupply;
+	}
+	
+	public TolerantSupplier<T> withAttemptCount(int attemptCount)
+	{
+		this.attemptCount = attemptCount;
+		return this;
+	}
+
+	public TolerantSupplier<T> withTimeToSleepValue(int timeToSleepValue,TimeUnit timeToSleepUnit)
+	{
+		this.timeToSleepValue = timeToSleepValue;
+		this.timeToSleepUnit = timeToSleepUnit;
+		return this;
+	}
+
+	public TolerantSupplier<T> withDefaultValue(T defaultValue)
+	{
+		this.defaultValue = defaultValue;
+		return this;
+	}
+	
+	@Override
+	public T get()
+	{
+		return TolerantSupplier.supplyPeriodicallyOrDefault(this.internSupplier, this.attemptCount, this.timeToSleepValue, this.timeToSleepUnit, this.defaultValue);
+	}
+
 	public static <T> T supplyPeriodicallyOrDefault(Supplier<T> supplier, int attemptCount, int timeToSleepValue, TimeUnit timeToSleepUnit, T defaultValue)
 	{
 		T suppliedValue = supplyPeriodically(supplier, attemptCount, timeToSleepValue, timeToSleepUnit);
---
+++ org.sodeac.common/src/test/java/org/sodeac/common/function/TolerantSupplierTest.java
@@ -0,0 +1,133 @@
+package org.sodeac.common.function;
+
+import static org.junit.Assert.assertEquals;
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
+
+public class TolerantSupplierTest
+{
+	
+	@Test
+	public void test01()
+	{
+		int i = 1;
+		int x = TolerantSupplier.supplyPeriodically(() -> i , 1, 1,TimeUnit.MILLISECONDS);
+		assertEquals("x should be corrext",i, x);
+	}
+	
+	
+	@Test
+	public void test02()
+	{
+		ConplierBean<Integer> i = new ConplierBean<Integer>(null);
+		Integer x = TolerantSupplier.supplyPeriodically(i , 1, 1,TimeUnit.MILLISECONDS);
+		assertEquals("x should be corrext",i.get(), x);
+	}
+	
+	@Test
+	public void test03()
+	{
+		ConplierBean<Integer> i = new ConplierBean<Integer>(null);
+		Integer x = TolerantSupplier.supplyPeriodicallyOrDefault(i , 3, 1,TimeUnit.SECONDS,5);
+		assertEquals("x should be corrext",Integer.valueOf(5), x);
+	}
+	
+	@Test
+	public void test04()
+	{
+		ConplierBean<Integer> i = new ConplierBean<Integer>(null);
+		
+		new Thread(() -> 
+		{ 
+			try{Thread.sleep(1000);}catch (Exception e) {}
+			i.setValue(5);
+		}).start();
+		
+		Integer x = TolerantSupplier.supplyPeriodicallyOrDefault(i , 3, 1,TimeUnit.SECONDS,3);
+		assertEquals("x should be corrext",Integer.valueOf(5), x);
+	}
+	
+	@Test
+	public void test05()
+	{
+		ConplierBean<Integer> i = new ConplierBean<Integer>(null);
+		
+		new Thread(() -> 
+		{ 
+			try{Thread.sleep(3000);}catch (Exception e) {}
+			i.setValue(5);
+		}).start();
+		
+		Integer x = TolerantSupplier.supplyPeriodicallyOrDefault(i , 3, 1,TimeUnit.SECONDS,3);
+		assertEquals("x should be corrext",Integer.valueOf(3), x);
+	}
+	
+	@Test
+	public void test11()
+	{
+		int i = 1;
+		int x = TolerantSupplier.forSupplier(() -> i).get();
+		assertEquals("x should be corrext",i, x);
+	}
+	
+	
+	@Test
+	public void test12()
+	{
+		ConplierBean<Integer> i = new ConplierBean<Integer>(null);
+		Integer x =  TolerantSupplier.forSupplier(i).get();
+		assertEquals("x should be corrext",i.get(), x);
+	}
+	
+	@Test
+	public void test13()
+	{
+		ConplierBean<Integer> i = new ConplierBean<Integer>(null);
+		Integer x = TolerantSupplier.forSupplier(i)
+					.withAttemptCount(3)
+					.withTimeToSleepValue(1, TimeUnit.SECONDS)
+					.withDefaultValue(5)
+					.get();
+		assertEquals("x should be corrext",Integer.valueOf(5), x);
+	}
+	
+	@Test
+	public void test14()
+	{
+		ConplierBean<Integer> i = new ConplierBean<Integer>(null);
+		
+		new Thread(() -> 
+		{ 
+			try{Thread.sleep(1000);}catch (Exception e) {}
+			i.setValue(5);
+		}).start();
+		
+		Integer x = TolerantSupplier.forSupplier(i)
+					.withAttemptCount(3)
+					.withTimeToSleepValue(1, TimeUnit.SECONDS)
+					.withDefaultValue(3)
+					.get();
+		assertEquals("x should be corrext",Integer.valueOf(5), x);
+	}
+	
+	@Test
+	public void test15()
+	{
+		ConplierBean<Integer> i = new ConplierBean<Integer>(null);
+		
+		new Thread(() -> 
+		{ 
+			try{Thread.sleep(3000);}catch (Exception e) {}
+			i.setValue(5);
+		}).start();
+		
+		Integer x = TolerantSupplier.forSupplier(i)
+					.withAttemptCount(3)
+					.withTimeToSleepValue(1, TimeUnit.SECONDS)
+					.withDefaultValue(3)
+					.get();
+		assertEquals("x should be corrext",Integer.valueOf(3), x);
+	}
+	
+	
+}
